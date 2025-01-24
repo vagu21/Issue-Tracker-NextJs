166c616aea575ac801110607142e6ce147c57246
vagu21 <bharadwaj21197@gmail.com>
2025-01-24 16:01:53 +0530

    addded a markdown editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="violet">
         <Navbar/>
         <main className="p-5">
         {children}
